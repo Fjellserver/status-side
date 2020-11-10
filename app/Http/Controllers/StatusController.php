78cd683ae2f11746a26932ed717a51b95c763061
BuildTools <bataldensondre@gmail.com>
2020-11-10 15:30:16 +0100

    db count host

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 class StatusController extends Controller
 {
     public function show() {
-
-    $status = \DB::table('status')->latest('last_checked')->take(6)->get();
-    $info = \DB::table('info')->latest('DateCreated')->take(4)->get();
+    
+    $hosts = \DB::table('hosts')->get();
+    $count = $hosts->count();
+    $status = \DB::table('status')->latest('created_at')->take($count)->get();
+    $info = \DB::table('info')->latest('created_at')->take(4)->get();
 
     //dd($status);
         return view('status', ['status' => $status], ['info' => $info]);
