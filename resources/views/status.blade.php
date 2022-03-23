49013f7b62b8fb30185cc16072e9042a2ba46a58
Pomdre <bataldensondre@gmail.com>
2022-03-23 19:22:37 +0100

    Systemstatus

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="theme-color" content="#1b316b"/>
-    <title>Status side| Fjellserver.no</title>
-    <meta name="description" content="Er serveren din nede? Sjekk status på systemet hos fjellserver.no">
+    <title>Systemstatus | Fjellserver.no</title>
+    <meta name="description" content="Er serveren din nede? Sjekk Systemstatus på systemer hos fjellserver.no">
     <meta property="og:type" content="website">
     <link rel="icon" type="image/png" sizes="36x36" href="https://fjellserver.no/assets/img/android-icon-36x36.png">
     <link rel="apple-touch-icon" sizes="180x180" href="https://fjellserver.no/assets/img/apple-touch-icon.png">
